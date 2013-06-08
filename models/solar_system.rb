fdda9924237b5b3cac98637ede27742a04b44246
Adam Kirkwood <adam@xyentific.com>
2013-06-08 14:01:46 -0700

    Wrapping mapSolarSystems with AR.
    Made convenience method to find a record's ID by Name

---
+++ models/solar_system.rb
@@ -0,0 +1,20 @@
+class SolarSystem < ActiveRecord::Base
+  self.table_name = "mapSolarSystems"
+  self.primary_key = "solarSystemID"
+  
+  alias_attribute :id, :solarSystemID
+  alias_attribute :name, :solarSystemName
+  alias_attribute :constellation_id, :constellationID
+  alias_attribute :region_id, :regionID
+  alias_attribute :security_class, :securityClass
+  
+  def as_json(options={})
+    options[:methods] = [:id, :name]
+    options[:only] = [:id, :name]
+    super
+  end
+  
+  def self.find_id_by_name(value)
+    select("solarSystemID").where("solarSystemName = ?", value).first.solarSystemID
+  end
+end
