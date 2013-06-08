63962ea56d8f080d346494ab50c56a6dcef27f4e
Adam Kirkwood <adam@xyentific.com>
2013-06-08 01:51:21 -0700

    Added querying by name. resolves #4

--- app.rb
+++ app.rb
@@ -42,13 +42,26 @@ end
 get '/structures' do
   content_type :json
   
-  response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
-    FROM invTypes
-    INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
-    WHERE invGroups.categoryID = 23 /*Structure*/
-        AND invTypes.groupID != 365 /*Exclude Control Towers*/
-        AND invTypes.published = 1 
-        AND invTypes.typeName NOT LIKE '%QA%'")
-  
-  return response.to_json
+  if params[:name]
+    response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
+      FROM invTypes
+      INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
+      WHERE invGroups.categoryID = 23 /*Structure*/
+          AND invTypes.groupID != 365 /*Exclude Control Towers*/
+          AND invTypes.published = 1 
+          AND invTypes.typeName NOT LIKE '%QA%'
+          AND invTypes.typeName LIKE '%#{params[:name]}%'")
+
+    return response.to_json
+  else
+    response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
+      FROM invTypes
+      INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
+      WHERE invGroups.categoryID = 23 /*Structure*/
+          AND invTypes.groupID != 365 /*Exclude Control Towers*/
+          AND invTypes.published = 1 
+          AND invTypes.typeName NOT LIKE '%QA%'")
+
+    return response.to_json
+  end
 end
