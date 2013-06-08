a07ab21c761d431b0038b87980f254a6bb3e8a3a
Adam Kirkwood <adam@xyentific.com>
2013-06-08 16:14:38 -0700

    Can query Celetials within a security range. Can retrieve Celestial by resource ID. Added limit query param.

--- models/celestial.rb
+++ models/celestial.rb
@@ -11,20 +11,37 @@ class Celestial < ActiveRecord::Base
   alias_attribute :region_id, :regionID
   alias_attribute :name, :itemName
   
+  default_scope select("mapDenormalize.itemName, mapDenormalize.itemID, mapDenormalize.groupID, mapDenormalize.solarSystemID, mapDenormalize.constellationID, mapDenormalize.regionID, ROUND(mapDenormalize.security, 1) as security")
+  
   scope :by_type, lambda { |value| where("mapDenormalize.groupID = ?", value) if value }
+  scope :by_id, lambda { |value| where("mapDenormalize.itemID = ?", value) if value }
   scope :within_solar_system, lambda { |value| where(:solarSystemID => SolarSystem.find_id_by_name(value)) if value }
   
   def as_json(options={})
-    options[:methods] = [:id, :name, :type_id, :group_id, :solar_system_id, :constellation_id, :region_id]
-    options[:only] = [:id, :name, :type_id, :group_id, :solar_system_id, :constellation_id, :region_id]
+    options[:methods] = [:id, :name, :type_id, :group_id, :solar_system_id, :constellation_id, :region_id, :security]
+    options[:only] = [:id, :name, :type_id, :group_id, :solar_system_id, :constellation_id, :region_id, :security]
     super
   end
   
   def self.search(params)
     celestials = Celestial.joins("INNER JOIN invGroups ON mapDenormalize.groupID = invGroups.groupID")
+                          .by_id(params[:id])
                           .within_solar_system(params[:solar_system])
                           .by_type(params[:type])
                           .limit(params[:limit])
+                          .with_security(params[:security])
+                          .limit(params[:limit])
     celestials
+  end
+  
+  def self.with_security(value)
+    return scoped if !value.present?
+    
+    if value =~ /,/
+      min, max = value.split(",")
+      where("ROUND(security, 1) >= ROUND(?, 1) AND ROUND(security, 1) <= ROUND(?, 1)", min, max) if value
+    else
+      where("ROUND(security, 1) = ROUND(?, 1)", value) if value
+    end
   end
 end
