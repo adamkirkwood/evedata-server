e8220442416d371219a26997cb9cf51e4163472c
Adam Kirkwood <adam@xyentific.com>
2013-06-08 03:30:25 -0700

    Added tux. Converted some queries to be more AR-like.

--- routes/star_routes.rb
+++ routes/star_routes.rb
@@ -2,19 +2,19 @@ get '/stars' do
   content_type :json
   
   if params[:id]
-    response = Star.find_by_sql("SELECT solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass FROM mapSolarSystems WHERE solarSystemID = #{params[:id]}")
+    response = Star.select("solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass").where("solarSystemID = ?", params[:id])
 
     return response.to_json
   elsif params[:name]
-    response = Star.find_by_sql("SELECT solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass FROM mapSolarSystems WHERE solarSystemName LIKE '%#{params[:name]}%'")
+    response = Star.select("solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass").where("solarSystemName LIKE ?", "%#{params[:name]}%")
 
     return response.to_json    
   elsif params[:security]
-    response = Star.find_by_sql("SELECT solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass FROM mapSolarSystems WHERE ROUND(security, 1) = ROUND(#{params[:security]}, 1)")
+    response = Star.select("solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass").where("ROUND(security, 1) = ROUND(?, 1)", params[:security])
 
     return response.to_json
   else
-    response = Star.find_by_sql("SELECT solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass FROM mapSolarSystems")
+    response = Star.select("solarSystemID, solarSystemName, ROUND(security, 1) as security, securityClass").all
 
     return response.to_json
   end
