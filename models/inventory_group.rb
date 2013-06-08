53232447154fa16adeac255901071863ef031088
Adam Kirkwood <adam@xyentific.com>
2013-06-08 12:16:49 -0700

    Wrapping invGroups with AR

---
+++ models/inventory_group.rb
@@ -0,0 +1,15 @@
+class InventoryGroup < ActiveRecord::Base
+  self.table_name = "invGroups"
+  self.primary_key = "groupID"
+  
+  alias_attribute :id, :groupID
+  alias_attribute :category_id, :categoryID
+  alias_attribute :name, :groupName
+  
+  def as_json(options={})
+    options[:methods] = [:id, :name]
+    options[:only] = [:id, :name]
+    super
+  end
+
+end
