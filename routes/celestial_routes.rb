722b8f2540c2c76c75d92648adca60fa2a983ff2
Adam Kirkwood <adam@xyentific.com>
2013-06-08 16:13:39 -0700

    Can GET resource by ID

--- routes/celestial_routes.rb
+++ routes/celestial_routes.rb
@@ -5,3 +5,11 @@ get '/celestials' do
 
   return response.to_json  
 end
+
+get '/celestials/:id' do
+  content_type :json
+  
+  response = Celestial.search(params)
+
+  return response.to_json  
+end
