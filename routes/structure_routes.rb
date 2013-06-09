d2327300b1c24bf575b1e7510ee2d99e012c6f5c
Adam Kirkwood <adam@xyentific.com>
2013-06-08 23:30:20 -0700

    DRY'd up the routes to use #search

--- routes/structure_routes.rb
+++ routes/structure_routes.rb
@@ -1,43 +1,15 @@
+get '/structures' do
+  content_type :json
+
+  response = Structure.search(params)
+
+  return response.to_json
+end
+
 get '/structures/:id' do
   content_type :json
 
-  if params[:id]
-    response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
-      FROM invTypes
-      INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
-      WHERE invGroups.categoryID = 23 /*Structure*/
-          AND invTypes.groupID != 365 /*Exclude Control Towers*/
-          AND invTypes.published = 1 
-          AND invTypes.typeName NOT LIKE '%QA%'
-          AND invTypes.typeID = #{params[:id]}")
+  response = Structure.by_id(params[:id])
 
-    return response.to_json
-  end
-end
-
-get '/structures' do
-  content_type :json
-  
-  if params[:name]
-    response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
-      FROM invTypes
-      INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
-      WHERE invGroups.categoryID = 23 /*Structure*/
-          AND invTypes.groupID != 365 /*Exclude Control Towers*/
-          AND invTypes.published = 1 
-          AND invTypes.typeName NOT LIKE '%QA%'
-          AND invTypes.typeName LIKE '%#{params[:name]}%'")
-
-    return response.to_json
-  else
-    response = Structure.find_by_sql("SELECT invTypes.typeID, invTypes.typeName
-      FROM invTypes
-      INNER JOIN invGroups ON invTypes.groupID = invGroups.groupID
-      WHERE invGroups.categoryID = 23 /*Structure*/
-          AND invTypes.groupID != 365 /*Exclude Control Towers*/
-          AND invTypes.published = 1 
-          AND invTypes.typeName NOT LIKE '%QA%'")
-
-    return response.to_json
-  end
+  return response.to_json
 end
