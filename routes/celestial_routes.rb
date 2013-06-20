865e4186c4f58f5c4abd54070e8e9d6fb77be27b
Adam Kirkwood <adam@xyentific.com>
2013-06-20 15:39:34 -0700

    Moved Celestial endpoints to new route file

--- routes/celestial_routes.rb
+++ routes/celestial_routes.rb
@@ -6,3 +6,12 @@ get '/celestials' do
     results.to_json
   end
 end
+
+get '/celestials/:id' do
+  content_type :json
+  
+  response ||= EveData::CacheManager.new.fetch(request.env['REQUEST_URI']) do
+    results = Celestial.search(params)
+    results.to_json
+  end
+end
