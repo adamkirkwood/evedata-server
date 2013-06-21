74c92d7b673ffae4ee024183eaf6877d837c89a2
Adam Kirkwood <adam@xyentific.com>
2013-06-21 12:09:26 -0700

    Removing grape

--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,3 @@
 ActiveRecord::Base.include_root_in_json = false
 
 WillPaginate.per_page = 25
-
-cache_options = {
-  :namespace => 'evedata',
-  :expires_in => 1.day,
-  :compress => true
-}
-$cache = Dalli::Client.new({}, cache_options)
