f164430409a399cea2fe62997cabc936cebbcf12
Adam Kirkwood <adam@xyentific.com>
2013-07-06 14:25:57 -0700

    Including additional item attributes

--- models/inventory_type.rb
+++ models/inventory_type.rb
@@ -5,6 +5,7 @@ class InventoryType < ActiveRecord::Base
   alias_attribute :id, :typeID
   alias_attribute :group_id, :groupID
   alias_attribute :name, :typeName
+  alias_attribute :base_price, :basePrice
   
   default_scope { where(:published => true) }
   
@@ -12,18 +13,23 @@ class InventoryType < ActiveRecord::Base
   scope :by_name, lambda { |value| where("typeName = ?", "#{value}") if value }
   
   def as_json(options={})
-    options[:methods] = [:id, :name]
-    options[:only] = [:id, :name]
+    options[:methods] = [:id, :name, :images]
+    options[:only] = [:id, :name, :base_price, :volume, :mass, :capacity, :description]
     super
   end
   
   def self.search(params)
-    puts params
     items = InventoryType.order(:typeID)
                          .by_id(params[:id])
                          .by_name(params[:name])
                          .paginate(:page => params[:page], :per_page => params[:limit])
-    puts items
     items
+  end
+  
+  def images
+    {
+      :small => "http://image.eveonline.com/Type/#{id}_32.png",
+      :thumb => "http://image.eveonline.com/Type/#{id}_64.png"
+    }
   end
 end
