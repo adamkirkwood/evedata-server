193a3485c2b62492797603e17a4763d1124320e1
Adam Kirkwood <adam@xyentific.com>
2013-07-06 13:52:41 -0700

    Allow searching through the InventoryType model

--- models/inventory_type.rb
+++ models/inventory_type.rb
@@ -6,9 +6,24 @@ class InventoryType < ActiveRecord::Base
   alias_attribute :group_id, :groupID
   alias_attribute :name, :typeName
   
+  default_scope { where(:published => true) }
+  
+  scope :by_id, lambda { |value| where("typeID = ?", value) if value }
+  scope :by_name, lambda { |value| where("typeName = ?", "#{value}") if value }
+  
   def as_json(options={})
     options[:methods] = [:id, :name]
     options[:only] = [:id, :name]
     super
+  end
+  
+  def self.search(params)
+    puts params
+    items = InventoryType.order(:typeID)
+                    .by_id(params[:id])
+                    .by_name(params[:name])
+                    .paginate(:page => params[:page], :per_page => params[:limit])
+    puts items
+    items
   end
 end
