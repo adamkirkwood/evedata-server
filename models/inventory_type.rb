146879e545d998f78959a3b03f6a5983c60c6bc5
Adam Kirkwood <adam@xyentific.com>
2013-07-06 14:51:22 -0700

    Including category name

--- models/inventory_type.rb
+++ models/inventory_type.rb
@@ -9,16 +9,17 @@ class InventoryType < ActiveRecord::Base
   alias_attribute :name, :typeName
   alias_attribute :base_price, :basePrice
   alias_attribute :group, :groupName
+  alias_attribute :category, :categoryName
   
   default_scope { where(:published => true) }
-  default_scope { select("invTypes.typeID, invTypes.groupID, invTypes.typeName, invTypes.basePrice, invTypes.volume, invTypes.mass, invTypes.capacity, invTypes.description, invGroups.groupName") }
+  default_scope { select("invTypes.typeID, invTypes.groupID, invTypes.typeName, invTypes.basePrice, invTypes.volume, invTypes.mass, invTypes.capacity, invTypes.description, invGroups.groupName, invCategories.categoryName") }
   
   scope :by_id, lambda { |value| where("typeID = ?", value) if value }
   scope :by_name, lambda { |value| where("typeName = ?", "#{value}") if value }
   
   def as_json(options={})
-    options[:methods] = [:id, :name, :images, :group]
-    options[:only] = [:id, :name, :base_price, :volume, :mass, :capacity, :description, :group]
+    options[:methods] = [:id, :name, :images, :group, :category]
+    options[:only] = [:id, :name, :base_price, :volume, :mass, :capacity, :description, :group, :category]
     super
   end
   
@@ -27,7 +28,9 @@ class InventoryType < ActiveRecord::Base
                          .by_id(params[:id])
                          .by_name(params[:name])
                          .joins("LEFT JOIN invGroups ON invTypes.groupID = invGroups.groupID")
+                         .joins("LEFT JOIN invCategories ON invGroups.categoryID = invCategories.categoryID")
                          .paginate(:page => params[:page], :per_page => params[:limit])
+
     items
   end
   
