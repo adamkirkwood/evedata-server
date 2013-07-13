b2d4c01375225f7437b412ea56b5319db31be960
Adam Kirkwood <adam@xyentific.com>
2013-07-13 00:18:09 -0700

    JSON now has group ID & name and category ID & name

--- models/inventory_type.rb
+++ models/inventory_type.rb
@@ -5,21 +5,24 @@ class InventoryType < ActiveRecord::Base
   has_one :inventory_group, :foreign_key => "groupID"
   
   alias_attribute :id, :typeID
-  alias_attribute :group_id, :groupID
   alias_attribute :name, :typeName
+  alias_attribute :group_id, :groupID
+  alias_attribute :group_name, :groupName
+  alias_attribute :category_name, :categoryName
+  alias_attribute :category_id, :categoryID
   alias_attribute :base_price, :basePrice
-  alias_attribute :group, :groupName
-  alias_attribute :category, :categoryName
   
   default_scope { where(:published => true) }
-  default_scope { select("invTypes.typeID, invTypes.groupID, invTypes.typeName, invTypes.basePrice, invTypes.volume, invTypes.mass, invTypes.capacity, invTypes.description, invGroups.groupName, invCategories.categoryName") }
+  default_scope { select("invTypes.typeID, invTypes.groupID, invTypes.typeName, invTypes.basePrice, invTypes.volume, invTypes.mass, invTypes.capacity, invTypes.description, invGroups.groupID, invGroups.groupName, invCategories.categoryName, invCategories.categoryID") }
   
   scope :by_id, lambda { |value| where("typeID = ?", value) if value }
   scope :by_name, lambda { |value| where("typeName = ?", "#{value}") if value }
+  scope :by_group, lambda { |value| where("groupName = ?", "#{value}") if value }
+  scope :by_group_name, lambda { |value| where("groupID = ?", "#{value}") if value }
   
   def as_json(options={})
     options[:methods] = [:id, :name, :images, :group, :category, :base_price]
-    options[:only] = [:id, :name, :base_price, :volume, :mass, :capacity, :description, :group, :category]
+    options[:only] = [:id, :name, :base_price, :volume, :mass, :capacity, :description]
     super
   end
   
@@ -38,6 +41,20 @@ class InventoryType < ActiveRecord::Base
     {
       :small => "http://image.eveonline.com/Type/#{id}_32.png",
       :thumb => "http://image.eveonline.com/Type/#{id}_64.png"
+    }
+  end
+  
+  def group
+    {
+      :id => group_id,
+      :name => group_name
+    }
+  end
+  
+  def category
+    {
+      :id => category_id,
+      :name => category_name
     }
   end
 end
