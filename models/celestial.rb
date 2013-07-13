d9cda0569dbe936da25a6869de536d5e9e37446b
Adam Kirkwood <adam@xyentific.com>
2013-07-13 03:11:41 -0700

    Changed JSON data structure for Celestials output

--- models/celestial.rb
+++ models/celestial.rb
@@ -5,15 +5,15 @@ class Celestial < ActiveRecord::Base
   belongs_to :inventory_group, :foreign_key => "groupID"
   
   alias_attribute :id, :itemID
-  alias_attribute :group_id, :groupID
-  alias_attribute :group, :groupName
-  alias_attribute :solar_system_id, :solarSystemID
-  alias_attribute :constellation_id, :constellationID
-  alias_attribute :region_id, :regionID
   alias_attribute :name, :itemName
-  alias_attribute :solar_system, :solarSystemName
-  alias_attribute :constellation, :constellationName
-  alias_attribute :region, :regionName
+  alias_attribute :group_id, :groupID
+  alias_attribute :group_name, :groupName
+  alias_attribute :solar_system_id, :solarSystemID
+  alias_attribute :solar_system_name, :solarSystemName
+  alias_attribute :constellation_id, :constellationID
+  alias_attribute :constellation_name, :constellationName
+  alias_attribute :region_id, :regionID
+  alias_attribute :region_name, :regionName
   
   default_scope select("mapDenormalize.itemName, mapDenormalize.itemID, mapDenormalize.groupID, mapDenormalize.solarSystemID, mapDenormalize.constellationID, mapDenormalize.regionID, ROUND(mapDenormalize.security, 1) as security, mapSolarSystems.solarSystemName, mapConstellations.constellationName, mapRegions.regionName, invGroups.groupName")
   
@@ -26,8 +26,8 @@ class Celestial < ActiveRecord::Base
   self.per_page = 25
   
   def as_json(options={})
-    options[:methods] = [:id, :name, :type_id, :group_id, :group, :solar_system_id, :constellation_id, :region_id, :security, :solar_system, :constellation, :region]
-    options[:only] = [:id, :name, :type_id, :group_id, :group, :solar_system_id, :constellation_id, :region_id, :security, :solar_system, :constellation, :region]
+    options[:methods] = [:id, :name, :type_id, :group, :security, :solar_system, :constellation, :region]
+    options[:only] = [:id, :name, :type_id, :security]
     super
   end
   
@@ -56,5 +56,33 @@ class Celestial < ActiveRecord::Base
     else
       where("ROUND(security, 1) = ROUND(?, 1)", value) if value
     end
+  end
+  
+  def group
+    {
+      :id => group_id,
+      :name => group_name
+    }
+  end
+  
+  def constellation
+    {
+      :id => constellation_id,
+      :name => constellation_name
+    }
+  end
+  
+  def region
+    {
+      :id => region_id,
+      :name => region_name
+    }
+  end
+  
+  def solar_system
+    {
+      :id => solar_system_id,
+      :name => solar_system_name
+    }
   end
 end
