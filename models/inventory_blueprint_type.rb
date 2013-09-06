22cc5d13482efe0693a80ce31eb06c7c5f284651
Bill Davis <bdavis84@pacbell.net>
2013-09-05 19:44:36 -0700

    added a research group to simplify json output

--- models/inventory_blueprint_type.rb
+++ models/inventory_blueprint_type.rb
@@ -16,10 +16,8 @@ class InventoryBlueprintType < ActiveRecord::Base
   alias_attribute :research_material_time, :researchMaterialTime
   alias_attribute :research_copy_time, :researchCopyTime
   alias_attribute :research_tech_time, :researchTechTime
-  
-  #Modifiers
-  alias_attribute :productivity_modifier, :productivityModifer
-  alias_attribute :material_modifier, :materialModifer
+  alias_attribute :productivity_modifier, :productivityModifier
+  alias_attribute :material_modifier, :materialModifier
   
   #invGroups Attributes
   alias_attribute :group_id, :groupID
@@ -40,10 +38,8 @@ class InventoryBlueprintType < ActiveRecord::Base
   
   def as_json(options={})
     options[:methods] = [:id, :name, :tech_level, :parent_id, :product_id, :production_time, :waste_factor, 
-    :max_production_limit, :research_productivity_time, :research_material_time, :research_copy_time, :research_tech_time,
-    :images, :group, :category]
-    options[:only] = [:id, :name, :tech_level, :parent_id, :product_id, :production_time, :waste_factor, 
-    :max_production_limit, :research_productivity_time, :research_material_time, :research_copy_time, :research_tech_time]
+    :max_production_limit, :images, :research, :group, :category]
+    options[:only] = [:id, :name, :tech_level, :parent_id, :product_id, :production_time, :waste_factor, :max_production_limit]
     super
   end
   
@@ -63,6 +59,17 @@ class InventoryBlueprintType < ActiveRecord::Base
     {
       :small => "http://image.eveonline.com/Type/#{id}_32.png",
       :thumb => "http://image.eveonline.com/Type/#{id}_64.png"
+    }
+  end
+  
+  def research
+    {
+      :productivity_time => research_productivity_time,
+      :material_time => research_material_time,
+      :copy_time => research_copy_time,
+      :tech_time => research_tech_time,
+      :productivity_modifier => productivity_modifier,
+      :material_modifier => material_modifier
     }
   end
   
