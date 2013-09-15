375accca7a6614ac9d0b5c4437a152013801c562
Adam Kirkwood <hello@adamkirkwood.com>
2013-09-15 10:02:34 -0700

    WIP: Puma Tuning

--- puma.rb
+++ puma.rb
@@ -1,3 +1,5 @@
+environment ENV['RACK_ENV']
+
 root = "#{Dir.getwd}"
 
 activate_control_app "tcp://127.0.0.1:9293"
@@ -7,3 +9,17 @@ rackup "#{root}/config.ru"
 state_path "#{root}/tmp/pids/puma.state"
 
 worker_processes 3
+timeout 3
+preload_app!
+threads 0,5
+
+on_worker_boot do
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL'] || 5
+    ActiveRecord::Base.establish_connection
+  end
+end
