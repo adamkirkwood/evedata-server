fa61bc6d0ec761839dfd3666a0c3a12513718a66
Adam Kirkwood <hello@adamkirkwood.com>
2013-09-15 10:18:22 -0700

    WIP: puma

--- puma.rb
+++ puma.rb
@@ -1,10 +1,10 @@
 root = "#{Dir.getwd}"
 
 # activate_control_app "tcp://127.0.0.1:9293"
-# bind "unix:///tmp/puma.pumatra.sock"
-# pidfile "#{root}/tmp/pids/puma.pid"
-# rackup "#{root}/config.ru"
-# state_path "#{root}/tmp/pids/puma.state"
+bind "unix:///tmp/puma.pumatra.sock"
+pidfile "#{root}/tmp/pids/puma.pid"
+rackup "#{root}/config.ru"
+state_path "#{root}/tmp/pids/puma.state"
 
 worker_processes 3
 timeout 3
