7dd9ee20f224f8869f2ae73c81601cf33a7da967
Adam Kirkwood <hello@adamkirkwood.com>
2013-09-15 10:46:41 -0700

    WIP: just AR

--- puma.rb
+++ puma.rb
@@ -9,11 +9,11 @@
 # state_path "#{root}/tmp/pids/puma.state"
 
 
+# workers 3
+# preload_app!
+# threads 0, 5
 
 on_worker_boot do
-  workers 3
-  preload_app!
-  threads 0, 5
 
   ActiveRecord::Base.connection_pool.disconnect!
 
